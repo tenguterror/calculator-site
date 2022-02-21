568241522cd9bb9d4d1070fbd5d77779e2da2531
Duard <vegasduard@gmail.com>
2022-02-20 17:19:56 -0700

    Basic functionalaity of calculator display

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ class Calculator {
     }
 
     appendNumber(number) {
-
+        this.currentOperand = number;
     }
 
     chooseOperation(operation) {
@@ -28,16 +28,26 @@ class Calculator {
     }
 
     updateDisplay() {
-
+        this.currentOperandTextElement.innerText =this.currentOperand;
     }
 }
 
 
-const numberButtons = document.querySelector('[data-number]');
-const operationButtons = document.querySelector('[data-operation]');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+
+
+numberButtons.forEach(button => {
+    button.onclick = () => {
+        calculator.appendNumber(button.innerHTML);
+        calculator.updateDisplay();
+    }
+})
